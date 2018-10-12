f1c5d630e35900f8804c4ab24daf392d6b99e7b9
Micimago <michaeljcini@hotmail.com>
2018-10-12 18:13:31 +0200

    First Commit

---
+++ precache-manifest.1fee57bb9987a408c3f308e2f194aa8b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "1bd2443a04a4f18abf10061c616fb064",
+    "url": "/static/media/background.1bd2443a.png"
+  },
+  {
+    "revision": "780ef5e14dc82cfab9f1",
+    "url": "/static/css/main.47a72221.chunk.css"
+  },
+  {
+    "revision": "4726bec1549ad25b4688034d73d0d62e",
+    "url": "/static/media/plane.4726bec1.png"
+  },
+  {
+    "revision": "1e100e37b03d7a26a469",
+    "url": "/static/js/1.1e100e37.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4284dfb0bf0b54f5e2b9a4790a4675f8",
+    "url": "/static/media/Clouds.4284dfb0.svg"
+  },
+  {
+    "revision": "780ef5e14dc82cfab9f1",
+    "url": "/static/js/main.780ef5e1.chunk.js"
+  },
+  {
+    "revision": "f9e053e73f1d1a6e63721646dba5e44c",
+    "url": "/static/media/buildings.f9e053e7.png"
+  },
+  {
+    "revision": "9c6316c3a44c690dcf24d9f34ee56d9e",
+    "url": "/static/media/iron-man.9c6316c3.png"
+  },
+  {
+    "revision": "bb0f0b1da6373c7832393129fa2d7519",
+    "url": "/static/media/background.bb0f0b1d.png"
+  },
+  {
+    "revision": "327b6aa169c2870151c48bb6ccde5f06",
+    "url": "/static/media/ground.327b6aa1.png"
+  },
+  {
+    "revision": "1e100e37b03d7a26a469",
+    "url": "/static/css/1.b1dcad14.chunk.css"
+  },
+  {
+    "revision": "84e4410f6523fc56af9e4d4f8bcfa07f",
+    "url": "/index.html"
+  }
+];
