8901df01fb6c83e0d9699f42ef28fe2321925b6e
Micimago <michaeljcini@hotmail.com>
2018-10-12 18:20:22 +0200

    Changed

---
+++ precache-manifest.01ab19e2e83ced45af374a7c0463a72f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "1bd2443a04a4f18abf10061c616fb064",
+    "url": "/animations/static/media/background.1bd2443a.png"
+  },
+  {
+    "revision": "df94f6e3e546d5a73ac2",
+    "url": "/animations/static/css/main.47a72221.chunk.css"
+  },
+  {
+    "revision": "4726bec1549ad25b4688034d73d0d62e",
+    "url": "/animations/static/media/plane.4726bec1.png"
+  },
+  {
+    "revision": "3ab02ed6eaba735d6df2",
+    "url": "/animations/static/js/1.3ab02ed6.chunk.js"
+  },
+  {
+    "revision": "7f86d48b0d8446893f1b",
+    "url": "/animations/static/js/runtime~main.7f86d48b.js"
+  },
+  {
+    "revision": "4284dfb0bf0b54f5e2b9a4790a4675f8",
+    "url": "/animations/static/media/Clouds.4284dfb0.svg"
+  },
+  {
+    "revision": "df94f6e3e546d5a73ac2",
+    "url": "/animations/static/js/main.df94f6e3.chunk.js"
+  },
+  {
+    "revision": "f9e053e73f1d1a6e63721646dba5e44c",
+    "url": "/animations/static/media/buildings.f9e053e7.png"
+  },
+  {
+    "revision": "9c6316c3a44c690dcf24d9f34ee56d9e",
+    "url": "/animations/static/media/iron-man.9c6316c3.png"
+  },
+  {
+    "revision": "bb0f0b1da6373c7832393129fa2d7519",
+    "url": "/animations/static/media/background.bb0f0b1d.png"
+  },
+  {
+    "revision": "327b6aa169c2870151c48bb6ccde5f06",
+    "url": "/animations/static/media/ground.327b6aa1.png"
+  },
+  {
+    "revision": "3ab02ed6eaba735d6df2",
+    "url": "/animations/static/css/1.b1dcad14.chunk.css"
+  },
+  {
+    "revision": "d1a7313e2abee0b959a0076b425cd5d2",
+    "url": "/animations/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1fee57bb9987a408c3f308e2f194aa8b.js"
+  "/animations/precache-manifest.01ab19e2e83ced45af374a7c0463a72f.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/animations/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
